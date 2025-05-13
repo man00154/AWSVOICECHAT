dd9e73643ef5cbdba91039508b98d870c5a244ad
man00154 <149796066+man00154@users.noreply.github.com>
2025-05-13 20:55:20 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+AWS_CONFIG = {
+    "aws_access_key_id": "your_access_key",
+    "aws_secret_access_key": "your_secret_key",
+    "region_name": "us-east-1"
+}
+
+LEX_CONFIG = {
+    "bot_name": "YourLexBotName",
+    "bot_alias": "YourLexBotAlias"
+}
